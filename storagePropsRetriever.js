ad6927ddead0346d96bd3795f2d5511bd2cc5d3b
LeasonYin1989 <838525178@qq.com>
2017-03-13 20:16:43 +0800

    Add files via upload

--- storagePropsRetriever.js
+++ storagePropsRetriever.js
@@ -5,7 +5,7 @@ const config = require('./config');
 function getStorageProps(targetService) {
     const accountName = serviceManager[targetService][config.region][config.level].storageName;
     const accountKey = serviceManager[targetService][config.region][config.level].storageKey;
-	const connection="DefaultEndpointsProtocol=https;AccountName=stroageservice;AccountKey=0DkZ+mgsije5hY3t+WeC+0NDWU45qCUt/92CNgxdLO2aE0NgA84jJCBtKRSyNh/L+qzhkgzmuvX1wLwvQvG5ug==;EndpointSuffix=core.chinacloudapi.cn";
+	const connection=serviceManager[targetService][config.region][config.level].storagetarget;
     return {
         accountName: accountName,
         accountKey: accountKey,

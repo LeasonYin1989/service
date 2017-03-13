aa23841f34c0efb8e274565da69f28a16de9e129
LeasonYin1989 <838525178@qq.com>
2017-03-13 20:20:35 +0800

    Update ReportAProblem.js

--- targets/ReportAProblem.js
+++ targets/ReportAProblem.js
@@ -1,14 +1,14 @@
 module.exports = {
 	na: {
 		dev: {
-			storageName: "bmwousiosanalyticsdev",
-		    storageKey: "0hGv24c/aZY2p4jDg3FXAwUyYP7G/mAr4IcvrGUT6k7A9bDOtbgQnzrEVvryBb5ZXpdTxvlhdnhQCRH525cDrw==",
+			storageName: "stroageservice",
+		    storageKey: "0DkZ+mgsije5hY3t+WeC+0NDWU45qCUt/92CNgxdLO2aE0NgA84jJCBtKRSyNh/L+qzhkgzmuvX1wLwvQvG5ug==",
 		    partitionKey: "RAP",
 		    tableName: "ReportAProblem",
 			storagetarget:"DefaultEndpointsProtocol=https;AccountName=stroageservice;AccountKey=0DkZ+mgsije5hY3t+WeC+0NDWU45qCUt/92CNgxdLO2aE0NgA84jJCBtKRSyNh/L+qzhkgzmuvX1wLwvQvG5ug==;EndpointSuffix=core.chinacloudapi.cn"
 		},
 		int: {
-			storageName: "bmwousiosanalyticsint",
+			storageName: "stroageservice",
 		    storageKey: "B7qtMLHaS2VeiMPxpalAGM7LI3rFiJQpC/WfHb6CIeKPdbGNVIeSkJUuHdtKRrCWwKIV0pw9xlXyxPailUbhhQ==",
 		    partitionKey: "RAP",
 		    tableName: "ReportAProblem",
